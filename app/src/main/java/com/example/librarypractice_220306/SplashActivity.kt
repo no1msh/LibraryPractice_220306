6e42b936f6738108337c743192e0cbc09826d526
TFQ_Moon <no1msh1217@gmail.com>
2022-03-06 13:34:05 +0900

    Splash 액티비티 추가

---
+++ app/src/main/java/com/example/librarypractice_220306/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.librarypractice_220306
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+
+}
